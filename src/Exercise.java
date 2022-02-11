45355b6fb9eba73586496f5132283fcfa390efae
batl64 <91633690+batl64@users.noreply.github.com>
2022-02-11 10:38:38 +0000

    2

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,6 +1,11 @@
 //package domain;
 
 public class Exercise {
+ /**
+ * Метод який приймаючи два числа создає масив в середені якого числа в діапазоні між цими числами які ми приймає, і виводи числа, які знаходяться в масиві через кому 
+ * @param A Початок діапазону
+ * @param B Кінець діапазону
+ */
     public static void Calculate(int A, int B){
       int []array = new int [(B-A)];
       int C=A;
