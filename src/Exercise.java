39fb427341322df3518672c38f081cacb9347228
batl64 <91633690+batl64@users.noreply.github.com>
2022-02-11 10:21:40 +0000

    1

---
+++ src/Exercise.java
@@ -0,0 +1,22 @@
+//package domain;
+
+public class Exercise {
+    public static void Calculate(int A, int B){
+      int []array = new int [(B-A)];
+      int C=A;
+      String res="";
+         for(int i=0;i<(B-A);i++){
+          array[i]=C;
+          C++;
+         }
+         for(int i=0;i<(B-A);i++){        
+          System.out.print(array[i]);
+          if(i != (B-A)-1){
+            System.out.print(",");
+          }
+          else{
+             System.out.print(".");
+          }          
+         }
+    }
+}
---
+++ src/TestResult.java
@@ -0,0 +1,13 @@
+//package test;
+
+//import domain.Exercise;
+
+public class TestResult {
+
+    public static void main(String[] args) {
+
+        int A=0,B=100;
+        System.out.println("An array of prime numbers with A="+A+" to B="+ B+" : ");
+        Exercise.Calculate(A,B);
+    }
+}
