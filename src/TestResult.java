e3572c5269ccfa2e63f56d7d5b8c458e0164736d
batl64 <91633690+batl64@users.noreply.github.com>
2022-02-16 22:21:34 +0200

    Update TestResult.java

--- src/TestResult.java
+++ src/TestResult.java
@@ -6,8 +6,9 @@ public class TestResult {
 
     public static void main(String[] args) {
 
-        int A=0,B=100;
+        int A=2,B=100;//A>1 B>A>1
         System.out.println("An array of prime numbers with A="+A+" to B="+ B+" : ");
         Exercise.Calculate(A,B);
     }
 }
+
